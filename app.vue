d466118ce4e2e2bf7edae1526e5bbf2ef34d1cc5
OziOcb <pozyzniewski@yahoo.com>
2022-10-14 13:14:40 +0100

    Add Meta tags

--- app.vue
+++ app.vue
@@ -4,6 +4,18 @@
   </NuxtLayout>
 </template>
 
+<script setup lang="ts">
+import { Meta } from '~/types/meta'
+const appConfig = useAppConfig()
+
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - ${appConfig.title}` : appConfig.title
+  },
+  meta: [{ name: 'description', content: appConfig.description }] as Meta[],
+})
+</script>
+
 <style lang="scss">
 body {
   background-color: $color-bg;
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,10 @@
 <template>
   <section><p>This page will be displayed at the /about route.</p></section>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'About Page',
+  meta: [{ name: 'description', content: 'About me page!' }],
+})
+</script>
